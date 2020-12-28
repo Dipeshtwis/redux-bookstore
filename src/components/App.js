21489cea61e7db8ed526d708c9c09df573209230
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-28 14:23:26 +0530

    Implement booklist component

--- src/components/App.js
+++ src/components/App.js
@@ -7,3 +7,5 @@ const App = () => (
     <BooksForm />
   </div>
 );
+
+export default App;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,18 @@
+const BooksList = () => {
+  return (
+     <table>
+       <tr>
+         <th>Book ID</th>
+         <th>title</th>
+         <th>category</th>
+       </tr>
+       <tr>
+         <td></td>
+         <td></td>
+         <td></td>
+       </tr>
+     </table> 
+  );
+}
+
+export default BooksList;
