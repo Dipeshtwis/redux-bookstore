49215054c10f52260a48f2a5d87ca25457c08ba8
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-28 13:43:48 +0530

    Implement app component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
+
+const App = () => (
+  <div className ='app'>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
