41192f7be846fff98af9c829fa9fdb823202faef
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-29 19:48:50 +0530

    Add handleclick function

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,19 +2,32 @@ import { useState } from 'react';
 
 const BooksForm = () => {
   // eslint-disable-next-line no-unused-vars
-  const category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const Category = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [state, setState] = useState({ title: '', category: '' });
+  const handleChange = ({ target: { name, value } }) => {
+    setState({ ...state, [name]: value });
+  };
+
   return (
     <>
       <h2>Add Book here</h2>
       <form>
         <label htmlFor="title">Book title:</label>
-        <input type="text" id="title" name="title" placeholder="Title of the book" />
-        <input type="checkbox" id="action" name="action" value="Bike" />
-        <label htmlFor="action"> Action</label>
-        <input type="checkbox" id="history" name="history" value="Car" />
-        <label htmlFor="history"> History</label>
-        <input type="checkbox" id="kids" name="kids" value="Boat" />
-        <label htmlFor="kids"> Kids</label>
+        <input type="text" onChange={handleChange} id="title" name="title" placeholder="Title of the book" required />
+        <select
+          onChange={handleChange}
+          name="category"
+          required
+        >
+          <option disabled value>
+            Select a category
+          </option>
+          {Category.map(category => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
         <button type="submit">Submit</button>
       </form>
     </>
