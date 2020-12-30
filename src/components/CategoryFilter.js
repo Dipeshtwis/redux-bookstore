0f06863e89997ffef0e1bcf0afdb82f5468a65ce
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-30 19:45:12 +0530

    Add react.framgment shortcut

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import allCategory from '../utils/utils';
 
 const CategoryFilter = ({ handleFilterChange }) => (
-  <div>
+  <>
     <h2>Select book with specific category</h2>
     <select
       onChange={handleFilterChange}
@@ -14,7 +14,7 @@ const CategoryFilter = ({ handleFilterChange }) => (
       </option>
       {allCategory}
     </select>
-  </div>
+  </>
 );
 
 CategoryFilter.propTypes = {
