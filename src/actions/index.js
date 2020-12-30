142577ccfd50048d40e3f09c29b7d2cd32da3ab6
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-30 14:57:03 +0530

    Add change filter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const createBookAction = book => ({
   type: CREATE_BOOK,
@@ -10,3 +11,8 @@ export const removeBookAction = book => ({
   type: REMOVE_BOOK,
   book,
 });
+
+export const changeFilterAction = filter => ({
+  type: CHANGE_FILTER,
+  filter,
+})
