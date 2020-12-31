97ee95867420b4c3b31d036d8b7eb630c7c09409
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-31 17:18:34 +0530

    Style left book row

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,23 +5,23 @@ const Book = ({ book, deleteBook }) => {
   return (
     <div className="book-row">
       <div>
-        <p>{category}</p>
+        <p className="book-para">{category}</p>
         <h3>{title}</h3>
-        <p>Frank Herbert</p>
-        <div>
-          <ul>
-            <li><button type="button">Comments</button></li>
-            <li><button type="button" onClick={deleteBook}>Delete</button></li>
-            <li><button type="button">Edit</button></li>
+        <p className="book-para text-blue">Frank Herbert</p>
+        <div className="book-row-left">
+          <ul className="action-list">
+            <li><button className="text-blue" type="button">Comments</button></li>
+            <li><button className="text-blue" type="button" onClick={deleteBook}>Delete</button></li>
+            <li><button className="text-blue" type="button">Edit</button></li>
           </ul>
         </div>
       </div>
-      <div>
+      <div className="book-row-middle">
         <div>Hey</div>
         <span>64%</span>
         <p>Completed</p>
       </div>
-      <div>
+      <div className="book-row-right">
         <p>CURRENT CHAPTER</p>
         <h4>Chapter 17</h4>
         <button type="button">UPDATE PROGRESS</button>
